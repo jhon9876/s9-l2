d6f20d6701c698bd776d8d5b16eb155bf0b0ddb7
Giovanni.iacono98 <98giovanniiacono98@gmail.com>
2024-07-03 17:35:16 +0200

    creazione di alcuni component e aggiunta file json

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from "./components/MyNav";
+import MyFooter from "./components/MyFooter";
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <div className="App">
+      <MyNav />
+      <Welcome />
+      <MyFooter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AllTheBooks.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class AllTheBooks extends Component{
+    render(){
+        return()
+    }
+}
+
+export default AllTheBooks
